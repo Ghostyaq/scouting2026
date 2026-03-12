0f6c3e869183adde5b30a9bfe6e8f01a3b4ddbc2
Ghostyaq <mitchellhung176@gmail.com>
2026-03-12 15:58:45 -0400

    rando wpilogs stuff, added the datasets to gitignore

--- wpilogs/fluxing_around.R
+++ wpilogs/fluxing_around.R
@@ -4,24 +4,26 @@ library(tidyverse)
 data <- read.csv("wpilogs/robot_data_qual70.csv")
 brownouts_only <- data[data$X.SystemStats.BrownedOut == 'true', ]
 data$left_shoot_vel <- as.numeric(data$X.Shooter.LeftLeaderVelocityRadPerSec)
+data$right_shoot_vel <- as.numeric(data$X.Shooter.RightLeaderVelocityRadPerSec)
 var_names <- data.frame(vars = names(data))
 shooting <- data[data$left_shoot_vel > 0,] |>
-    select(Timestamp, left_shoot_vel) |>
-    #filter(Timestamp > 170) |>
-    filter(left_shoot_vel > 150) |>
+    select(Timestamp, left_shoot_vel, right_shoot_vel) |>
+    filter(Timestamp > 190) |>
+    filter(left_shoot_vel > 130) |>
+    filter(right_shoot_vel > 130) |>
     filter(!is.na(left_shoot_vel))
 
-shooting$z_scores <- scale(shooting$left_shoot_vel)
+shooting$left_z_scores = scale(shooting$left_shoot_vel)
+shooting$right_z_scores = scale(shooting$right_shoot_vel)
 
-ggplot(shooting, aes(x = Timestamp, y = left_shoot_vel)) + 
-    geom_line() + 
-    scale_y_continuous(breaks = seq(0, 300, by = 20)) +
+ggplot(shooting, aes(x = Timestamp)) + 
+    geom_point(aes(y = left_z_scores), color = "#a7000a", size = 0.1) +
+    geom_point(aes(y = right_z_scores), color = "blue", size = 0.1) + 
+    scale_y_continuous() + 
+    #scale_y_continuous(breaks = seq(0, 300, by = 20)) +
     theme_bw()
 
-sum(shooting$z_scores < -1.35) # 65 balls shot for < -2, 293 for < -1
+sum(shooting$left_z_scores < -4) # 65 balls shot for < -2, 293 for < -1
+sum(shooting$right_z_scores < -4)
 
 # 154 shot during the entirety of qual 70
-
-ggplot(data, aes(x = Timestamp, y = left_shoot_vel)) +
-    geom_line() + 
-    theme_bw()
