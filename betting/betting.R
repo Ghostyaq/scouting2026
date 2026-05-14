3a0be5901cf229eb4c819eddf3e6c8ea9c6049a0
Mitchell <94205674+Ghostyaq@users.noreply.github.com>
2026-05-14 12:53:37 -0400

    Scout motivation (#38)
    * made gambling system using statbotics odds
    
    * changed it to automatically get match winner from statbotics, oops
    
    ---------
    
    Co-authored-by: Jack <jp10davis@gmail.com>

--- betting/betting.R
+++ betting/betting.R
@@ -7,17 +7,20 @@ starting_balance = 1000
 odds <- data.frame(match_number = 1:max(bets$match_number), 
                    chance_red = 0, chance_blue = 0, red_mult = 0, blue_mult = 0)
 
+winner_is_red <- FALSE
+
 for(i in 1:max(bets$match_number)){
     odds$chance_red[i] = match_sb(paste0(event_key, "_qm", as.character(i)))$pred$red_win_prob
     odds$chance_blue[i] = 1 - odds$chance_red[i]
     odds$red_mult[i] = 1/odds$chance_red[i]
     odds$blue_mult[i] = 1/odds$chance_blue[i]
+    winner_is_red[i] <- match_sb(paste0(event_key, "_qm", as.character(i)))$result$winner == "red"
 }
 
-winner_is_red <- filter(bets, scout_initials == "DS")$prediction == "Red"
-
-bets <- mutate(bets, prediction_is_red = prediction == "Red", winner_is_red = winner_is_red[match_number]) |> 
-    select(scout_initials, match_number, prediction_is_red, winner_is_red, point_bet)
+bets <- mutate(bets, prediction_is_red = prediction == "Red", 
+               winner_is_red = winner_is_red[match_number]) |> 
+    select(scout_initials, match_number, 
+           prediction_is_red, winner_is_red, point_bet)
 
 bets <- bets |> mutate(delta = ifelse(xor(winner_is_red, prediction_is_red),
                               point_bet * -1,
